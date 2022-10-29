35e9c3b5af3469c16c97201cfe55485d6043fa9b
Jack Collison <jcollison97@gmail.com>
2022-10-29 11:52:42 -0500

    Update some functionality
    - Add simple contraction mapping
    - Need to handle special cases

--- Demand.jl
+++ Demand.jl
@@ -22,7 +22,7 @@ function Λ(δ::Array{Float64}, X::Array{Float64}, ν::Array{Float64}, Σ::Array
         x = δ + (ν[i,:] .* X) * Σ
 
         # Check for demographics
-        if Π != nothing && Π != nothing
+        if D !== nothing && Π !== nothing
             # Add demographics
             x += sum((D[i, :] .* X) * Π, dims=2)
         end
@@ -37,25 +37,49 @@ end
 
 # Simulated shares
 function σ(δ::Array{Float64}, X::Array{Float64}, ν::Array{Float64}, Σ::Array{Float64}, R::Int64; D::Array{Float64}=nothing, Π::Array{Float64}=nothing)
-    # Return brand-wise mean of individual market shares
-    mean(Λ(δ, X, ν, Σ, R, D=D, Π=Π), dims=1)
+    # Return predicted shares
+    return mean(Λ(δ, X, ν, Σ, R, D=D, Π=Π), dims=1)
 end
 
 # Contraction mapping within a market
-function contraction_mapping_market(δ::Array{Float64}, δ₀::Array{Float64}, X::Array{Float64}, log_share::Array{Float64}, ν::Array{Float64}, Σ::Array{Float64}, R::Float64; tol::Float64=1e-10, verbose::Bool=false, D::Array{Float64}=nothing, Π::Array{Float64}=nothing)
-    return nothing
+function contraction_mapping_market(δ::Array{Float64}, δ₀::Array{Float64}, X::Array{Float64}, shares::Array{Float64}, ν::Array{Float64}, Σ::Array{Float64}, R::Float64; tol::Float64=1e-10, maxiter::Int64=1000, D::Array{Float64}=nothing, Π::Array{Float64}=nothing)
+    # Initialize counter
+    i = 0
+
+    # Iterate while error is large
+    while maximum(abs.(δ .- δ₀)) > tol && i <= maxiter
+        # Increment counter
+        i += 1
+
+        # Update previous and iterate
+        δ₀ = δ
+        δ += log.(shares) - σ(δ, X, ν, Σ, R, D=D, Π=Π)
+    end
+
+    # Return value
+    return δ
 end
 
 # Contraction mapping
-function contraction_mapping(δ₀::Array{Float64}, X::Array{Float64}, shares::Array{Float64}, ν::Array{Float64}, Σ::Array{Float64}, markets::Array{Float64}; tol::Float64=1e-10, verbose::Bool=false, D::Array{Float64}=nothing, Π::Array{Float64}=nothing)
-    return nothing
+function contraction_mapping(δ₀::Array{Float64}, X::Array{Float64}, shares::Array{Float64}, ν::Array{Float64}, Σ::Array{Float64}, markets::Array{Float64}; tol::Float64=1e-10, maxiter::Int64=1000, verbose::Bool=false, D::Array{Float64}=nothing, Π::Array{Float64}=nothing)
+    # Initialize δ
+    δ = zeros(size(markets, 1))
+
+    # Iterate over unique markets
+    # TODO: Parallelize if it's more efficient
+    for (m, market) in enumerate(unique(markets))
+        # Filter to market and contract
+        index = (markets .== market)
+        δ[index] = contraction_mapping_market(δ[index], δ₀[index], X[index,:], shares[index], ν[m,:,:], Σ, R, tol=tol, maxiter=maxiter, D=D[m,:,:], Π=Π)
+        # TODO: Handle case when D, Π are nothing objects because we can't apply the index mask
+    end
 end
 
 # Objective function
 function gmm(θ₂::Array{Float64}, W::Array{Float64}, ν::Array{Float64}; D::Array{Float64}=nothing, index::Array{Float64}=nothing)
     # TODO: Reshape θ₂ into Σ and Π
     # TODO: Ensure only values at index are entered
-    # Note: Index is 4x5 and θ₂ is Kx1 where K is the number of non-zero elements of Σ and Π
+    # Note: Index is 4x5 binary matrix and θ₂ is of length K, where K is the number of non-zero elements of Σ and Π
     return nothing
 end
 
