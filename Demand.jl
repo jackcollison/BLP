5b650a2868c1aae69355b3f7f1ce9bebe705ec63
Jack Collison <jcollison97@gmail.com>
2022-10-30 20:02:02 -0500

    First stage GMM function
    - Still needs to be tested
    - Check indexing for omitted variables

--- Demand.jl
+++ Demand.jl
@@ -1,5 +1,6 @@
 # Import required libraries
 using Parameters, LinearAlgebra
+include("Utilities.jl")
 
 # Multinomial logit function
 function p(x)
@@ -73,14 +74,47 @@ function contraction_mapping(δ₀::Array{Float64}, X::Array{Float64}, shares::A
         δ[index] = contraction_mapping_market(δ[index], δ₀[index], X[index,:], shares[index], ν[m,:,:], Σ, R, tol=tol, maxiter=maxiter, D=D[m,:,:], Π=Π)
         # TODO: Handle case when D, Π are nothing objects because we can't apply the index mask
     end
+
+    # Return value
+    return δ
 end
 
 # Objective function
-function gmm(θ₂::Array{Float64}, W::Array{Float64}, ν::Array{Float64}; D::Array{Float64}=nothing, index::Array{Float64}=nothing)
-    # TODO: Reshape θ₂ into Σ and Π
-    # TODO: Ensure only values at index are entered
+function gmm(θ₂::Array{Float64}, X₁::Array{Float64}, X₂::Array{Float64}, Z::Array{Float64}, shares::Array{Float64}, δ₀::Array{Float64}, markets::Array{Float64}, W::Array{Float64}, ν::Array{Float64}; D::Array{Float64}=nothing, index::Array{Float64}=nothing, verbose::Bool=false, tol::Float64=1e-10)
     # Note: Index is 4x5 binary matrix and θ₂ is of length K, where K is the number of non-zero elements of Σ and Π
-    return nothing
+    # TODO: Test indexing works
+
+    # Initialize Σ and Π
+    Σ = θ₂[1:size(X1, 2)]
+    Π = nothing
+    if size(θ₂) > size(X₁, 2)
+        Π = zeros(size(index))
+        counter = 1
+        for i = 1:size(Π, 1)
+            for j = 1:size(Π, 2)
+                if index[i, j] > 0
+                    Π[i, j] = θ₂[size(X1, 2) + counter]
+                    counter += 1
+                end
+            end
+        end
+    end
+
+    # Contraction mapping
+    δ = contraction_mapping(δ₀, X₂, shares, ν, Σ, markets, tol=tol, maxiter=maxiter, verbose=verbose, D=D, Π=Π)
+
+    # Fit linear model
+    m = fit(X₁, δ, Z, "IVGMM")
+    θ₁ = m.θ
+    s₁ = m.s
+
+    # Obtain residuals and compute GMM criterion
+    ξ = δ - X₁ * θ₁
+    g = mean(Z * ξ, dims=1)
+    val = g.T * W * g
+
+    # Return value
+    return val, θ₁, s₁, ξ
 end
 
 # Jacobian
@@ -95,10 +129,20 @@ end
 
 # Full model
 #X1, X2, Z, share, markets, ν, D, R, Σ, Π, δ_0, method='L-BFGS-B', tol_1=1e-5, tol_2=1e-10, verbose=False
-function blp(x)
-    return nothing
+function blp(results::Demand)
+    # Print statement
+    println("Beginning to fit BLP model...")
+
+    # Objective function wrapper
+    function obj(θ₂)
+        # Call original GMM criterion
+        val, results.θ₁, results.s₁, ξ = gmm(θ₂, X₁, X₂, Z, shares, δ₀, markets, W, ν, D=D, index=index, verbose=verbose, tol=tol)
+    end
 end
 
 # Structure for results
 struct Demand
+    # Results
+    θ₁::Array{Float64}
+    s₁::Array{Float64}
 end
